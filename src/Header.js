e2f5aa1c77dbc008848b5bb778e78c5f76a061a3
2036380 <m.zandoamos@wlv.ac.uk>
2022-05-25 16:07:05 +0100

    another commit

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ import { providers } from "ethers";
 const Header = () => {
     const [accounts, setAccounts] = useState([]); 
     const isConnected = JSON.parse(localStorage.getItem("connectionStatus"));
-
+console.log(isConnected)
         async function connectAccount() {
             if(window.ethereum)  {
                 const accounts = await window.ethereum.request({
@@ -45,16 +45,15 @@ const Header = () => {
                 <Container fluid className="conta">
                     <Navbar.Brand href="../"><Image src={logo} width="220px" height="40px" className="" alt="logo"/></Navbar.Brand>             
                     <Navbar.Toggle aria-controls="offcanvasNavbar" className='text-white bg-warning'/>
-                    <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end"  className="Offcanvascss bg-transparent" >
-                    {/* <div className='bg-nav-overlay'></div> */}
-                    <Offcanvas.Header closeButton style={{color: 'white'}} className='text-white porelative'>
+                    <Navbar.Offcanvas id="" className="bg-dark" aria-labelledby="offcanvasNavbarLabel" variant="" placement="end" >
+                    <Offcanvas.Header closeButton className='porelative'>
                         <Offcanvas.Title id="offcanvasNavbarLabel">FingerMonkey</Offcanvas.Title>
                     </Offcanvas.Header>
-                    <Offcanvas.Body className="porelative">
+                    <Offcanvas.Body className="bg-transparent">
                         <Nav className="justify-content-end flex-grow-1 pe-3 text-dark">
-                            <Nav.Link href="../" className="nav-link text-white">HOME</Nav.Link>
-                            <Nav.Link href="../#about-us" className="nav-link text-white font-weight-bold">ABOUT</Nav.Link>
-                            <Nav.Link href="../#faq" className="nav-link text-white">FAQ</Nav.Link>
+                            <Nav.Link href="../" className="nav-link text-dark">HOME</Nav.Link>
+                            <Nav.Link href="../#about-us" className="nav-link text-dark font-weight-bold">ABOUT</Nav.Link>
+                            <Nav.Link href="../#faq" className="nav-link text-dark">FAQ</Nav.Link>
                             <Nav.Link href="../#mintnft" ><Button className="nav-link text-dark bg-warning p-1 px-4 outline-success">MINT NFT</Button></Nav.Link>
                             {isConnected ? (<Button className="nav-link text-dark bg-warning p-1 outline-success">WALLET CONNECTED</Button>) : (<Button onClick={connectAccount} className="nav-link text-dark bg-warning p-1 outline-none">CONNECT WALLET</Button>)}
                         </Nav>
